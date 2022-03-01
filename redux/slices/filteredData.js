91df45909884aba2d0f5e02b433399c57a9581cd
eribau <erik_bauer@hotmail.com>
2022-03-01 22:20:51 +0100

    Add basic redux store setup

---
+++ redux/slices/filteredData.js
@@ -0,0 +1,10 @@
+import speciesData from "../../public/allDataByAssessmentId.json"
+
+const filteredData = (state = speciesData, action) => {
+   switch(action.type) {
+      default:
+         return state
+   }
+}
+
+export default filteredData
