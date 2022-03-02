340ed6120b006082dc3c76599d0b56a0e6381edd
eribau <erik_bauer@hotmail.com>
2022-03-02 14:29:29 +0100

    Remove redux-subscriber

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,10 @@
 import { createStore } from 'redux'
 import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducer'
-import initSubscriber from 'redux-subscriber'
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(),
   );
-
-initSubscriber(store)
 
 export default store;
