76b66b0c79dc69e2c15ba898b61f6eec81a9a35d
anna <anna>
2022-03-02 14:14:39 +0100

    updates in useState hook

--- redux/store.js
+++ redux/store.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux'
 import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducer'
-import initSubscriber from 'redux-subscriber'
+//import initSubscriber from 'redux-subscriber'
 
 const store = createStore(
     rootReducer,
     composeWithDevTools(),
   );
 
-initSubscriber(store)
+//initSubscriber(store)
 
 export default store;
