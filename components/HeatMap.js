3a08f255c8aaaa331aa91a6137018007b40a8f4c
estolpe <estolpe@kth.se>
2022-03-02 11:58:34 +0100

    bump

--- components/HeatMap.js
+++ components/HeatMap.js
@@ -15,15 +15,6 @@ import { useChartDimensions } from '../utilities/useChartDimensions'
 import store from '../redux/store'
 import { setFilteredData } from '../redux/slices/filteredData'
 
-// TODO: This page is temporary, heatmap should be included as part of the 
-// index.js map. So, this should be implemented into WorldMap.js later.
-
-// TODO: Color gradient legend
-// https://www.visualcinnamon.com/2016/05/smooth-color-legend-d3-svg-gradient/
-// http://bl.ocks.org/nbremer/5cd07f2cb4ad202a9facfbd5d2bc842e
-
-// Blockbuilder.org is very useful for prototyping.
-
 const mouseOver = (d, i) => {
     d3.select('#' + d.target.id)
         .attr('opacity', '.5');
@@ -35,6 +26,8 @@ const mouseLeave = (d, i) => {
         .attr('opacity', '1');
 };
 const HeatMap = (props) => {
+
+    /// PopupWindow & Filtering ///
     const [displayBox, setDisplay] = useState(false);
     const [code, setCode] = useState("");
     const [country, setCountry] = useState("");
@@ -50,33 +43,27 @@ const HeatMap = (props) => {
         setCode(d.target.id)
         setDisplay(true)
     };
+
     const closeWindow = () => {
         setDisplay(false)
-    }
+    };
+
     function changeFilter(){
         if (dispayFilter)
             setdispayFilter(false)
         else
             setdispayFilter(true)
     }
+
     function onCategoryChanges(value){
-        console.log(value)
         store.dispatch(setFilteredData(value))
         setCategory(value)
 
         updateHeatmap(value["category"]);
     }
 
-    const dimensions = {
-        'width': 1400,
-        'height': 1000,
-        'marginTop': 20,
-        'marginRight': 10
-      }
-    
-    
-       // grab our custom React hook we defined above
-       const [ref, dms] = useChartDimensions(dimensions)
+    /// PopupWindow & Filtering ///
+
 
     function updateHeatmap(checked){
         const interpolation = d3.interpolate({colors: ["#FFFFFF"]}, {colors: ["#db000f"]});
@@ -129,7 +116,16 @@ const HeatMap = (props) => {
         d3.select("rect").selectChild("title").text("0 - " + maxSpeciesAggregated);
     }
 
-    // Need this for d3 to work with react.
+    /// Heatmap ///
+    const dimensions = {
+        'width': 1400,
+        'height': 1000,
+        'marginTop': 20,
+        'marginRight': 10
+      }
+
+    const [ref, dms] = useChartDimensions(dimensions)
+
     useEffect( () => {
 
         const svg = d3.select('svg');
@@ -139,116 +135,93 @@ const HeatMap = (props) => {
         // #3d0006 #d43547 #db000f
         const interpolation = d3.interpolate({colors: ["#FFFFFF"]}, {colors: ["#db000f"]});
         var centered = false;
-        // Zoom did not work for some reason
-        // const g = svg.append('g');
-
-        // g.append('path')
-        // .attr('class', 'sphere')
-        // .attr('d', pathGenerator({type: 'Sphere'}));
-
-
-        // Loading multiple data sets at once
-        Promise.all([
-            //d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv')
-            //d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') using local data now
-            //d3.tsv('../public/topo.id.info.tsv') how do I get the file to work locally?
-            //d3.json('../public/topo.json')
-        ]).then(([tsv, topoJSONdata]) => { // destructuring syntax (packing up multiple values)
-            tsv = tsvData;
-            //topoJSONdata = topoJSONdataTemp;
-            //console.log(topoJSONdata);
             
-            // Testing with local data, seems to work, TODO: clean up code when both files work locally...
-            topoJSONdata = topoJSONdataTemp;
+        // Testing with local data, seems to work, TODO: clean up code when both files work locally...
+        topoJSONdata = topoJSONdataTemp;
             
-            //console.log(topoJSONdataTemp);
+        //console.log(topoJSONdataTemp);
 
-            const speciesPerCountryJSONdata = speciesPerCountryCode; // ~250 entries
+        const speciesPerCountryJSONdata = speciesPerCountryCode; // ~250 entries
             
-            // HEATMAP STUFF
-            // Get array of species lengths.
-            const numSpeciesByCountry = {};
+        // HEATMAP STUFF
+        // Get array of species lengths.
+        const numSpeciesByCountry = {};
 
-            let maxSpecies = 0;
-            for (const prop in speciesPerCountryJSONdata){
-                const numSpecies = speciesPerCountryJSONdata[prop].length;
-                if (numSpecies > maxSpecies){
-                    maxSpecies = numSpecies;
+        let maxSpecies = 0;
+        for (const prop in speciesPerCountryJSONdata){
+            const numSpecies = speciesPerCountryJSONdata[prop].length;
+            if (numSpecies > maxSpecies){
+                maxSpecies = numSpecies;
+            }
+            // prop is country code iso_a2
+            numSpeciesByCountry[prop] = numSpecies;
+        }
+        const getColor = (v) => {
+            const scaledValue = v / maxSpecies;
+            return interpolation(scaledValue).colors;
+        };
+
+        //console.log(numSpeciesByCountry);
+        //console.log(speciesPerCountryJSONdata); // 250 entries
+        //console.log(tsvData); // 177 entries
+        //console.log(topoJSONdata); // 177 countries
+        // Construct lookup table for id:s to names.
+        // That is, from iso_n3 to: country name, and iso_a2
+        const countryNamesByTopoId = {};
+        tsvData.forEach(d => {
+
+            // TODO: just edit the data later, these if statements are temporary?
+            // Some countries do not have an iso_n3 or iso_a2 code. 
+            if (d.iso_a2 == "-99"){
+                console.log(d.name);
+                console.log(d.iso_n3);
+                // I have set the ids in the topo.json file to
+                // Kosovo:101, Somaliland:102, N.Cyprus:103
+                // If we want to remove the ifs, then we need to edit in the tsv file as well
+                // but then we need to use it locally first.
+                if (d.name == "Somaliland"){
+                    countryNamesByTopoId["102"] = ["AAB", d.name]; // self proclaimed, I set it to "AAB"
+                } else if (d.name == "Kosovo") {
+                    countryNamesByTopoId["101"] = ["XK", d.name]; // XK is the official one for Kosovo
+                } else if (d.name == "N. Cyprus"){
+                    countryNamesByTopoId["103"] = ["ABB", d.name]; // self proclaimed, I set it to "ABB"
                 }
-                // prop is country code iso_a2
-                numSpeciesByCountry[prop] = numSpecies;
+            }
+            else{
+                countryNamesByTopoId[d.iso_n3] = [d.iso_a2, d.name];
+                //console.log( countryNamesByTopoId[d.iso_n3][1])
             }
 
-            const getColor = (v) => {
-                const scaledValue = v / maxSpecies;
-                return interpolation(scaledValue).colors;
-            };
-
-            //console.log(numSpeciesByCountry);
-            //console.log(speciesPerCountryJSONdata); // 250 entries
-            //console.log(tsvData); // 177 entries
-            //console.log(topoJSONdata); // 177 countries
-
-            // Construct lookup table for id:s to names.
-            // That is, from iso_n3 to: country name, and iso_a2
-            const countryNamesByTopoId = {};
-            tsvData.forEach(d => {
-
-                // TODO: just edit the data later, these if statements are temporary?
-                // Some countries do not have an iso_n3 or iso_a2 code. 
-                if (d.iso_a2 == "-99"){
-                    console.log(d.name);
-                    console.log(d.iso_n3);
-                    // I have set the ids in the topo.json file to
-                    // Kosovo:101, Somaliland:102, N.Cyprus:103
-                    // If we want to remove the ifs, then we need to edit in the tsv file as well
-                    // but then we need to use it locally first.
-                    if (d.name == "Somaliland"){
-                        countryNamesByTopoId["102"] = ["AAB", d.name]; // self proclaimed, I set it to "AAB"
-                    } else if (d.name == "Kosovo") {
-                        countryNamesByTopoId["101"] = ["XK", d.name]; // XK is the official one for Kosovo
-                    } else if (d.name == "N. Cyprus"){
-                        countryNamesByTopoId["103"] = ["ABB", d.name]; // self proclaimed, I set it to "ABB"
-                    }
-
-                }
-                else{
-                    countryNamesByTopoId[d.iso_n3] = [d.iso_a2, d.name];
-                    //console.log( countryNamesByTopoId[d.iso_n3][1])
-                }
-
-            });
-
-            // Use topojson to get the features from the topoJSONdata that we want
-            const countries = feature(topoJSONdata, topoJSONdata.objects.countries);
-            
-            //console.log(countries);
-
-            svg.selectAll('path').data(countries.features)
-            .enter().append('path')
-            .attr('d', d => pathGenerator(d))
-            .attr('id', d => countryNamesByTopoId[d.id][0]) // <- iso_a2
-            .attr('name', d => countryNamesByTopoId[d.id][1]) // <- name
-            .attr('fill', d => {
-                const countryCode = countryNamesByTopoId[d.id][0];
-                const numSpecies = numSpeciesByCountry[countryCode];
-
-                return '' + getColor(numSpecies);
-            }) 
-            .attr('stroke', '#212226')
-            .attr('stroke-opacity', '0.25')
-            .on('mouseover', mouseOver)
-            .on('mouseleave', mouseLeave)
-            .on('click', onClick)
-            .lower()
-            .append('title').text(d => {
-                // d.id is iso_n3, which is what is used to identify each country topology in topo.json
-                const countryCode = countryNamesByTopoId[d.id][0]; // <-  [0] is iso_a2
-                const countryName = countryNamesByTopoId[d.id][1]; // <-  [1] is country name
-                const numSpecies = numSpeciesByCountry[countryCode];
-                return countryName + ":" + numSpecies;
-            });
         });
+
+        // Use topojson to get the features from the topoJSONdata that we want
+        const countries = feature(topoJSONdata, topoJSONdata.objects.countries);
+        
+        //console.log(countries);
+        svg.selectAll('path').data(countries.features)
+        .enter().append('path')
+        .attr('d', d => pathGenerator(d))
+        .attr('id', d => countryNamesByTopoId[d.id][0]) // <- iso_a2
+        .attr('name', d => countryNamesByTopoId[d.id][1]) // <- name
+        .attr('fill', d => {
+            const countryCode = countryNamesByTopoId[d.id][0];
+            const numSpecies = numSpeciesByCountry[countryCode];
+            return '' + getColor(numSpecies);
+        }) 
+        .attr('stroke', '#212226')
+        .attr('stroke-opacity', '0.25')
+        .on('mouseover', mouseOver)
+        .on('mouseleave', mouseLeave)
+        .on('click', onClick)
+        .lower()
+        .append('title').text(d => {
+            // d.id is iso_n3, which is what is used to identify each country topology in topo.json
+            const countryCode = countryNamesByTopoId[d.id][0]; // <-  [0] is iso_a2
+            const countryName = countryNamesByTopoId[d.id][1]; // <-  [1] is country name
+            const numSpecies = numSpeciesByCountry[countryCode];
+            return countryName + ":" + numSpecies;
+        });
+        
 
                 // svg.call(d3.zoom().on('zoom', () => {
         //     svg.attr('transform', d3.zoomTransform(this));
