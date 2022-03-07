9796eb4ef8c85ed8c276ac17686b2cae467f3618
estolpe <estolpe@kth.se>
2022-03-07 17:13:39 +0100

    Add selectedCountry to reducer

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 
 import filteredData from "./slices/filteredData"
+import selectedCountry from "./slices/selectedCountry"
 
 const rootReducer = combineReducers({
    filteredData,
+   selectedCountry
 })
 
 export default rootReducer
