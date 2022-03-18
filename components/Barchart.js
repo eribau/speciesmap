b86e7040b00382cfdfd39795512a3113ed1576bf
eribau <erik_bauer@hotmail.com>
2022-03-18 15:18:35 +0100

    Add legend to bar chart

--- components/Barchart.js
+++ components/Barchart.js
@@ -330,6 +330,29 @@ const Barchart = (props) => {
         .keys(subgroups)
         (data)
 
+        const legend = svg.append("g")
+        legend.selectAll("dots")
+            .data(subgroups.slice(0, 8))
+            .enter()
+            .append("circle")
+            .attr("cx", 100)
+            .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+            .attr("r", 7)
+            .style("fill", function(d){ return color(d)})
+
+        legend.selectAll("labels")
+            .data(subgroups.slice(0, 8))
+            .enter()
+            .append("text")
+            .attr("x", 120)
+            .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+            .style("fill", "white")
+            .text(function(d){ return d})
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle")
+
+        legend.attr("transform", "translate(1100, -50)")
+
         // ----------------
         // Create a tooltip
         // ----------------
